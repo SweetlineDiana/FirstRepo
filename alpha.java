b71e417ceeb988d82fcdaeb608cb5894e370ce7c
Sweetline <sweetline.diana@wipro.com>
2023-07-29 19:45:41 +0530

    FirstCommit

---
+++ alpha.java
@@ -0,0 +1,11 @@
+package com.testDemo;
+
+public class TestDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hey, Welcome to JenkinsDemo!");
+
+	}
+
+}
